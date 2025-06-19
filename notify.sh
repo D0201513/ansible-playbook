e087e138d803f4c7a2aec22d9d68078d9375b254
D0201513 <aravind_slcs_intern2@aravind.org>
2025-06-19 09:33:52 +0530

    Update notify.sh

--- notify.sh
+++ notify.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# File: /path/to/notify.sh
-
 LOG_FILE="/tmp/notify_helper.log"
 CURRENT_DATE=$(date '+%Y-%m-%d %H:%M:%S')
 HOSTNAME=$(hostname)
@@ -11,10 +9,7 @@ log() {
     echo "[$CURRENT_DATE] $1" >> "$LOG_FILE"
 }
 
-# Start logging
 log "🔁 notify.sh executed on $HOSTNAME"
-
-# System info log (customize if needed)
 OS_INFO=$(grep '^PRETTY_NAME=' /etc/os-release | cut -d= -f2 | tr -d '"')
 log "🖥️  System Info: $OS_INFO"
 log "🕒 Timestamp: $CURRENT_DATE"
